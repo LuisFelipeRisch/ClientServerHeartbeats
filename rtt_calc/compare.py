93b0aa4eecb26348053b62b9f63deaf04f89c244
Luis Risch <luis.risch@gmail.com>
2025-10-15 20:17:29 -0300

    changes

--- rtt_calc/compare.py
+++ rtt_calc/compare.py
@@ -56,6 +56,7 @@ class TuningPhiTimeoutCalculator:
     self.amount_hits             = 0
     self.amount_misses           = 0
     self.timeouts_over_time      = []
+    self.mean_mistake            = -1
   
   def __update_estimated_an_deviation_rtt(self, current_rtt): 
     if self.estimated_rtt == -1 and self.deviation_rtt == -1: 
@@ -73,6 +74,11 @@ class TuningPhiTimeoutCalculator:
       self.amount_hits += 1
     else:
       self.amount_misses += 1
+
+      if self.mean_mistake == -1:
+        self.mean_mistake = current_rtt - last_timeout
+      else: 
+        self.mean_mistake = (self.ALPHA * self.mean_mistake) + (1 - self.ALPHA) * (current_rtt - last_timeout)
   
   def print_statistics(self):
     print("Tuning Phi statistics:")
@@ -105,7 +111,9 @@ class TuningPhiTimeoutCalculator:
     else:
       calculated_phi = math.ceil(abs(((trend + self.deviation_rtt) - self.estimated_rtt) / float(self.deviation_rtt)))
 
-    self.timeouts_over_time.append(self.estimated_rtt + calculated_phi * self.deviation_rtt)
+    timeout = self.estimated_rtt + calculated_phi * self.deviation_rtt
+
+    self.timeouts_over_time.append(timeout)
 
 class TuningPhiV2TimeoutCalculator: 
   ALPHA     = 0.9
@@ -161,7 +169,7 @@ tun_phi_v2_timeout_calculator = TuningPhiV2TimeoutCalculator()
 times                         = []
 rtts                          = []
 
-with open("./data.txt") as file: 
+with open("./data2.txt") as file: 
   for i, line in enumerate(file, start=1): 
     try:
       current_rtt = float(line.strip())
@@ -175,23 +183,30 @@ with open("./data.txt") as file:
       print(f"Erro: linha {i} não contém um número válido: {line.strip()}")
       sys.exit(1)
 
+
+for x in range(len(rtts)):
+  if x == 0: 
+    print(f'RTT: {rtts[x]} | TIMEOUT JAC: - | TIMEOUT Tun Phi: -')
+  else: 
+    print(f'RTT: {rtts[x]} | TIMEOUT JAC: {jac_timeout_calculator.timeouts_over_time[x - 1]} | TIMEOUT Tun Phi: {tun_phi_timeout_calculator.timeouts_over_time[x - 1]}')
+
 jac_timeout_calculator.print_statistics()
 tun_phi_timeout_calculator.print_statistics()
-tun_phi_v2_timeout_calculator.print_statistics()
+# tun_phi_v2_timeout_calculator.print_statistics()
 
-plt.plot(times, rtts, marker='o', linestyle='--', color='b', label='Actual Rtt')
-plt.plot([t + 1 for t in times], jac_timeout_calculator.timeouts_over_time, marker='s', linestyle='-', color='g', label='Timeout by Jac')
-plt.plot([t + 1 for t in times], tun_phi_timeout_calculator.timeouts_over_time, marker='^', linestyle=':', color='r', label='Timeout By Tun Phi')
-plt.plot([t + 1 for t in times], tun_phi_v2_timeout_calculator.timeouts_over_time, marker='d', linestyle='-.', color='m', label='Timeout By Tun Phi V2')
+# plt.plot(times, rtts, marker='o', linestyle='--', color='b', label='Actual Rtt')
+# plt.plot([t + 1 for t in times], jac_timeout_calculator.timeouts_over_time, marker='s', linestyle='-', color='g', label='Timeout by Jac')
+# plt.plot([t + 1 for t in times], tun_phi_timeout_calculator.timeouts_over_time, marker='^', linestyle=':', color='r', label='Timeout By Tun Phi')
+# plt.plot([t + 1 for t in times], tun_phi_v2_timeout_calculator.timeouts_over_time, marker='d', linestyle='-.', color='m', label='Timeout By Tun Phi V2')
 
-plt.title("Jacobson X Tuning PHI X Tuning PHI V2")
-plt.xlabel("Time")
-plt.ylabel("Rtt")
+# plt.title("Jacobson X Tuning PHI X Tuning PHI V2")
+# plt.xlabel("Time")
+# plt.ylabel("Rtt")
 
-ax = plt.gca() 
-ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+# ax = plt.gca() 
+# ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
-plt.grid(True)
-plt.legend()
-plt.show()
+# plt.grid(True)
+# plt.legend()
+# plt.show()
 
